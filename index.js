2f5a20d3863875061cd6c1b58d9de43ad5636c1c
Abhay khandal <abhaykhandal2111@gmail.com>
2023-08-24 10:58:08 +0530

    code committed

--- index.js
+++ index.js
@@ -4,25 +4,38 @@ const port = 5000;
 app.use(express.json());
 var admin = require("firebase-admin");
 var serviceAccount = require("./serviceAccountKey.json");
-
+const functions = require('firebase-functions');
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://reactjs-project-b7b3a-default-rtdb.firebaseio.com"
 });
+const db = admin.database();
 
-app.get('/', (req, res) => {
+
+app.post('/addData', (req, res) => {
     const db = admin.database();
-    res.send('Hello, World!');
-    const dataToAdd = {
-        name: 'John Doe',
-        email: 'john@example.com',
-        age: 30
-    };
+    const data = req.body; 
     const ref = db.ref('usersData');
-    const newRef = ref.push(dataToAdd);
-    console.log('New data added with key:', newRef.key);
+
+    ref.push(data, (error) => {
+        if (error) {
+            res.status(500).send('Data could not be added.');
+        } else {
+            res.status(200).send('Data added successfully.');
+        }
+    });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+ 
+
+app.get('/users', async (req, res) => {
+    const usersRef = db.ref('usersData');
+    usersRef.get().then((data) => {
+    return res.status(201).json(data);
+
+    })
+})
+
+app.listen(port, function () {
+    exports.app = functions.https.onRequest(app);
+})
