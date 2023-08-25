d0aa86769b00d88d4a001d9ed248b5b7c20297e5
Abhay khandal <abhaykhandal2111@gmail.com>
2023-08-25 13:20:36 +0530

    code committed

--- index.js
+++ index.js
@@ -2,22 +2,28 @@ const express = require('express');
 const app = express();
 const port = 5000;
 app.use(express.json());
-var admin = require("firebase-admin");
-var serviceAccount = require("./serviceAccountKey.json");
-const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const serviceAccount = require("./serviceAccountKey.json");
+const functions = require('firebase-functions')
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://reactjs-project-b7b3a-default-rtdb.firebaseio.com"
+    databaseURL: "https://wevois-office-management.firebaseio.com/"
+    
 });
-const db = admin.database();
+// const db = admin.database();
 
 
-app.post('/addData', (req, res) => {
+app.post('/Authentication',(req, res) => {
+    const userId = req.body.username;
     const db = admin.database();
-    const data = req.body; 
-    const ref = db.ref('usersData');
-
-    ref.push(data, (error) => {
+    if (!req.body.username) {
+        return res.status(401).send('Unauthorized');
+    }
+   
+    // const data = req.body; 
+    const data={password:req.body.password,username:req.body.username};
+    const ref = db.ref('Authentication');
+    ref.child(userId).update(data, (error) => {
         if (error) {
             res.status(500).send('Data could not be added.');
         } else {
@@ -28,8 +34,9 @@ app.post('/addData', (req, res) => {
 
  
 
-app.get('/users', async (req, res) => {
-    const usersRef = db.ref('usersData');
+app.get('/Authentication', async (req, res) => {
+    const db = admin.database();
+    const usersRef = db.ref('Authentication');
     usersRef.get().then((data) => {
     return res.status(201).json(data);
 
@@ -37,5 +44,6 @@ app.get('/users', async (req, res) => {
 })
 
 app.listen(port, function () {
+    console.log(`Server is running on port ${port}`);
     exports.app = functions.https.onRequest(app);
 })
