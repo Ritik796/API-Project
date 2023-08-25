7a9c0f1b7cca0e85f402d01f055ce887ebd804d7
Abhay khandal <abhaykhandal2111@gmail.com>
2023-08-25 15:25:14 +0530

    code committed

--- functions/index.js
+++ functions/index.js
@@ -8,8 +8,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const { onRequest } = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+// const { onRequest } = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -18,3 +18,10 @@ const logger = require("firebase-functions/logger");
 //     logger.info("Hello logs!", { structuredData: true });
 //     response.send("Hello from Firebase!");
 // });
+const functions=require("firebase-functions");
+const express=require("express");
+const app=express();
+app.get("/",(req,res)=>{
+    res.status(200).send({data:"Hello"})
+});
+exports.app=functions.https.onRequest(app);
